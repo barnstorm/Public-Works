247f92e624e016118020fb5be957cd61654faa03
barnstorm <wrarmstrong@gmail.com>
2016-08-02 16:29:38 -0600

    Work!

--- scavengerstrail/character.py
+++ scavengerstrail/character.py
@@ -9,28 +9,45 @@ class Character(object):
         self.maxload = maxload
         self.hp = hp
         self.inv = list()
-    class Inventory(object):
-        def __init__(self):
-            for i in inv:
-
-            free = self.maxload - self.load
-
-        def addInventory(self,item):
-            if avail > item[u'load']:
-                self.inv.append(item)
-                print('added ' + item[u'load'] + ' to inventory')
-
     def isAlive(self):
         return self.hp > 0
+    def inventory(self, action,item=''):
+        myinv = Inventory(self.inv, self.maxload)
+        if action == 'add':
+            if item == '':
+                return 'nothing to add'
+            return myinv.addInventory(item)
+        if action == 'drop':
+            if item == '':
+                return 'nothing to drop'
+            return myinv.dropInventory(item)
+        if action == 'show':
+            return myinv.showInventory()
 
-    def dropInventory(self):
-
-
-
+class Inventory(object):
+    def __init__(self, inv, maxload):
+        self.inv = inv
+        self.maxload = maxload
+        self.load = sum(i[u'load'] for i in self.inv)
+        self.avail = self.maxload - self.load
+    def addInventory(self,item):
+        if self.avail == 0:
+            return 'you cannot carry anymore'
+        if self.avail >= item[u'load']:
+            self.inv.append(item)
+            return 'added ' + str(item[u'name']) + ' to inventory'
+        else:
+            return 'You only have ' + str(self.avail) + ' load available'
+    def showInventory(self):
+        return self.inv
+    def dropInventory(self, dropitem):
+        self.inv.remove(dropitem)
+        return dropitem['name'] + 'Dropped'
 
 class Trader(Character):
     def __init__(self, cunning, reputation):
         self.cunning = cunning
         self.reputation = reputation
+        super(Trader,self).__init__(attack=7,defense=5,hp=3,description='desc',name='Char',maxload=200, load=0)
 
 class Player(Trader):
--- scavengerstrail/items.py
+++ scavengerstrail/items.py
@@ -8,6 +8,7 @@ class Item(object):
 
 class Consumable(Item):
     def __init__(self, **itemDict):
+        self.count = itemDict[u'count']
         self.title = itemDict[u'title']
         self.reqs = itemDict[u'reqs']
         super(Consumable,self).__init__(name=itemDict[u'name'],description=itemDict[u'description'],load=itemDict[u'load'],value=itemDict[u'value'])
@@ -15,8 +16,8 @@ class Consumable(Item):
 
 class Weapon(Consumable):
     def __init__(self, **weapDict):
-        self.title = itemDict[u'title']
-        self.reqs = itemDict[u'reqs']
-        super(Weapon,self).__init__(name=itemDict[u'name'],description=itemDict[u'description'],load=itemDict[u'load'],value=itemDict[u'value'])
+        self.title = weapDict[u'title']
+        self.reqs = weapDict[u'reqs']
+        super(Weapon,self).__init__(name=weapDict[u'name'],description=weapDict[u'description'],load=weapDict[u'load'],value=weapDict[u'value'])
 
 
