ee44721cf33502f1e78827bf4fe8dc04b8895ed9
bills_mac <bill@armstravaganza.com>
2016-09-28 22:14:45 -0600

    NEW! Chatbot

--- Homebot/Bot.py
+++ Homebot/Bot.py
@@ -3,7 +3,7 @@ import time
 import re
 import os
 import json
-from Homebot.libs.Worker import *
+from Worker import *
 
 class Dict2Obj(object):
     """
@@ -18,53 +18,46 @@ class Dict2Obj(object):
         """"""
         return "<Dict2Obj: %s>" % self.__dict__
 
-mp3savedir = '/home/barmstrong/homebot/mp3/'
+class Chat(object):
+    def __init__(self, username='user', password='passwd', server=(server, 5223)):
+        self.username = username
+        self.password = password
+        self.server = server
 
-if not os.path.exists(mp3savedir):
-    os.makedirs(mp3savedir)
+    def message_handler(self, connect_object, message_node):
+        us = str(message_node.getFrom()).split('/')[0]
+        if us == 'bill@armstravaganza.com':
+            us = us[0:4]
+            print str(message_node)
+            s= str(message_node.getBody()).replace("\n", "\t")
+            if s != 'None' :
+                mp3 = re.compile('#getaudio$')
+                vid = re.compile('#getvideo$')
+                toamazon = re.compile('#toamazon$')
+                print "MESSAGE: " + s
+                if mp3.findall(s):
+                    url = mp3.split(s)[0]
+                    res = "transcoding " + url
+                    connect_object.send(xmpp.Message( message_node.getFrom() , res, typ='chat' ))
+                    title = getaudio(url)
+                    finished = "Finished " + res + title
+                    connect_object.send(xmpp.Message( message_node.getFrom() , finished, typ='chat' ))
+                else:
+                    nothing = 'Nothing to do with:\n' + s
+                    print "Nothing"
+                    connect_object.send(xmpp.Message( message_node.getFrom() ,nothing, typ='chat' ))
 
-user="homebot@armstravaganza.com"
-password="!@qwASzx"
-server=('talk.google.com', 5223)
-def message_handler(connect_object, message_node):
-    us = str(message_node.getFrom()).split('/')[0]
-    if us == 'bill@armstravaganza.com':
-        us = us[0:4]
-        print str(message_node)
-        message = "Welcome to my first Gtalk Bot :) " + us
-        s= str(message_node.getBody()).replace("\n", "\t")
-        if s <> 'None' :
-            mp3 = re.compile('#getaudio$')
-            vid = re.compile('#getvideo$')
-            toamazon = re.compile('#toamazon$')
-            print "MESSAGE: " + s
-            if mp3.findall(s):
-                url = mp3.split(s)[0]
-                res = "transcoding " + url
-                connect_object.send(xmpp.Message( message_node.getFrom() , res, typ='chat' ))
-                title = getaudio(url)
-                finished = "Finished " + res + title
-                connect_object.send(xmpp.Message( message_node.getFrom() , finished, typ='chat' ))
-            else:
-                nothing = 'Nothing to do with:\n' + s
-                print "Nothing"
-                connect_object.send(xmpp.Message( message_node.getFrom() ,nothing, typ='chat' ))
-jid = xmpp.JID(user)
-connection = xmpp.Client(jid.getDomain())
-connection.connect(server)
-result = connection.auth(jid.getNode(), password )
-
-connection.RegisterHandler('message', message_handler)
-connection.sendInitPresence()
-
-while connection.Process(1):
-    pass
+    def communicator(self):
+        jid = xmpp.JID(self.username)
+        connection = xmpp.Client(jid.getDomain())
+        connection.connect(self.server)
+        result = connection.auth(jid.getNode(), password )
+        connection.RegisterHandler('message', self.message_handler)
+        connection.sendInitPresence()
+        while connection.Process(1):
+            pass
 
 if __name__ == '__main__':
-    import json
-    with open('config.json', 'r') as f:
-        configfile = json.load(f)
-        CONFIG = Dict2Obj(config)
-        BOTUSER = CONFIG.MAIN['BOTUSER']
-        BOTPASS = CONFIG.MAIN['BOTPASS']
-        COMMANDS = CONFIG.COMMANDS
+    username="homebot@armstravaganza.com"
+    password="!@qwASzx"
+    server=('talk.google.com', 5223)
